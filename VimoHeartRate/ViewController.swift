2c3018864cc4e6f13f22f13f3ffe2caf93e6ba15
Ethan Fan <coolioxlr@gmail.com>
2015-06-25 12:34:24 -0700

    Initial Commit

---
+++ VimoHeartRate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VimoHeartRate
+//
+//  Created by Ethan Fan on 6/25/15.
+//  Copyright © 2015 Vimo Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
